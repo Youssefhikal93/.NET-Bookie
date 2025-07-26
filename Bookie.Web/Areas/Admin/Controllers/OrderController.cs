9eced57aafb36204d8ca33de33a2eba9121f3e15
youssef <youssefhikal@gmail.com>
2025-07-26 23:04:39 +0200

    Added filter to order index

--- Bookie.Web/Areas/Admin/Controllers/OrderController.cs
+++ Bookie.Web/Areas/Admin/Controllers/OrderController.cs
@@ -1,8 +1,12 @@
 ﻿using Bookie.DataAccess.Repository.IRepository;
+using Bookie.Models;
+using Bookie.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BookieWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -17,10 +21,22 @@ namespace BookieWeb.Areas.Admin.Controllers
         }
 
         #region API CALLS
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            var OrderHeadersList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> OrderHeadersList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
 
+            switch (status)
+            {
+                case "pending": 
+                    OrderHeadersList = OrderHeadersList.Where(u=>u.PaymentStatus==SD.PaymentStatusDelayedPayment); break;
+                case "inprocess":
+                    OrderHeadersList = OrderHeadersList.Where(u => u.PaymentStatus == SD.StatusInProcess); break;
+                case "completed":
+                    OrderHeadersList = OrderHeadersList.Where(u => u.PaymentStatus == SD.StatusShipped); break;
+                case "approved":
+                    OrderHeadersList = OrderHeadersList.Where(u => u.PaymentStatus == SD.StatusApproved); break;
+                default: break;
+            }
 
             return Json(new { data = OrderHeadersList });
         }
---
+++ Bookie.Web/wwwroot/js/order.js
@@ -0,0 +1,59 @@
+﻿var dataTable;
+
+function loadDataTable(status) {
+   
+    dataTable = $('#myTable').DataTable({
+        "ajax": {
+            url: '/admin/order/getall?status='+status,
+            dataSrc: 'data'
+        },
+        "columns": [
+            { "title": "ID", data: 'id' },
+            { "title": "Name", data: 'applicationUser.name' },
+            { "title": "Phone Number", data: 'applicationUser.phoneNumber' },
+            { "title": "Email", data: 'applicationUser.email' },
+            { "title": "Status", data: 'orderStatus' },
+            { "title": "Total", data: 'orderTotal' },
+            {
+                "title": "Actions",
+                data: 'id',
+                render: function (data) {
+                    return `
+                        <div class="w-100 btn-group gap-2" role="group">
+                            <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-1">
+                                <i class="bi bi-list px-2"></i>view
+                            </a>
+                           
+                        </div>
+                    `;
+                }
+            }
+        ]
+    });
+}
+
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+
+        loadDataTable("inprocess");
+    } else if (url.includes("completed")) {
+
+        loadDataTable("completed");
+    }
+
+    else if (url.includes("pending")) {
+
+        loadDataTable("pending");
+    }
+    else if (url.includes("approved")) {
+
+        loadDataTable("approved");
+    } else {
+
+        loadDataTable("all");
+    }
+
+
+});
