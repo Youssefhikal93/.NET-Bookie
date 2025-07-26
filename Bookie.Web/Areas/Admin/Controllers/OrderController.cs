51f1dc4b6ea9ecac917ba0f63bd35bf864b402fe
Youssef Hikal <youssefhikal@gmail.com>
2025-07-26 15:39:11 +0200

    Added OrderVM and OrderController (#4)
    * Stripe_Configration
    
    * AddedSecretKeys
    
    * Update appsettings.json
    
    * Added shoopingCart and order details
    
    * Revert "Added shoopingCart and order details"
    
    * Stripe_Configration
    
    * AddedSecretKeys

---
+++ Bookie.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Bookie.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookieWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        public IActionResult GetAll()
+        {
+            var OrderHeadersList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+
+            return Json(new { data = OrderHeadersList });
+        }
+        #endregion
+    }
+}
